9f912dee3cd13e2007460a8929d6fea88e80ce8e
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-03-25 16:41:23 +0300

    25.03.2023

---
+++ src/day16_forLoop/C02_ForLoopHatalar.java
@@ -0,0 +1,22 @@
+package day16_forLoop;
+
+public class C02_ForLoopHatalar {
+    public static void main(String[] args) {
+        /*
+        Baslangic noktasindan sonra bitis sartina yaklasmiyorsa
+        sonsuz loop olusur.
+            for (int i = 0; i > -10; i++) {
+                       System.out.print(i);}
+         */
+
+
+        /*
+        eger ilk deger icin bile bitis sarti saglamiyorsa
+            - for loop calisir
+            - ama loop body'si hicbir zaman devreye girmez (calismaz)
+            for (int i = 0; i > 5; i++) {
+            System.out.println(i);}
+         */
+
+    }
+}
