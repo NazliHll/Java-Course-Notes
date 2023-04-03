bf03911a7c884f48d98165ea4e88206d20bdfe0f
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-03 18:02:27 +0300

    03.04.2023

---
+++ src/day29_PassByeValue_Immutable/C04_immutable.java
@@ -0,0 +1,34 @@
+package day29_PassByeValue_Immutable;
+
+public class C04_immutable {
+    public static void main(String[] args) {
+         /*
+        Immutable : degistirilemez
+        mutable   : degistirilebilir
+        --> en meshur immutable class : String
+         */
+
+        String str="Yildiz Bank";
+        System.out.println(str.toUpperCase());//YİLDİZ BANK
+
+        str.toLowerCase();
+        System.out.println(str);//Yildiz Bank
+
+        str.substring(3,5);
+        System.out.println(str);//Yildiz Bank
+
+        /*
+        Java'da string gibi metin ifadelerde kullanabileceğimiz
+        mutable StringBuilder class'i da vardir.
+         */
+        StringBuilder sb=new StringBuilder("Java Bank");
+        System.out.println(sb);//Java Bank
+
+        sb.reverse();
+        System.out.println(sb);//knaB avaJ
+
+        sb.append(".");
+        System.out.println(sb);//knaB avaJ.
+
+    }
+}
