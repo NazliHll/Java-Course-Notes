7c59ecd484931ce84060736470e7c23a186cc6c7
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-05 11:47:49 +0300

    05.04.2023

---
+++ src/day32_StringBuilder/C02_Append.java
@@ -0,0 +1,21 @@
+package day32_StringBuilder;
+
+public class C02_Append {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Java daha ne yapsin");
+        sb.append("?");
+        System.out.println(sb);//Java daha ne yapsin?
+
+         /*
+        Append istedigimiz String'i en sona ekler
+         */
+        sb.insert(4," her seyi dusunmus,");
+        System.out.println(sb);//Java her seyi dusunmus, daha ne yapsin?
+
+        /*
+        Araya ekleme yapmak istedimizde append degil insert kullanmaliyiz.
+         */
+
+
+    }
+}
---
+++ src/day32_StringBuilder/C03_Reverse.java
@@ -0,0 +1,15 @@
+package day32_StringBuilder;
+
+public class C03_Reverse {
+    public static void main(String[] args) {
+           /*
+        Verilen bir input'u tersine cevirip bize donduren bir method olusturun.
+         */
+        String input="Hey gidi for loop gunleri";
+        String tersInput=tersineCevir(input);
+    }
+
+    public static String tersineCevir(String input) {
+
+    }
+}
