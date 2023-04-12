cf5b7d1bade776548265d16032ca4ce4b98c274a
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-12 17:21:47 +0300

    12.04.2023

---
+++ src/day40_final/CAbstract.java
@@ -0,0 +1,22 @@
+package day40_final;
+
+public class CAbstract {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*
+    Bir method'un basina abstract yazarsaniz bu method'un child class'lar icin bir
+    standard oldugunu declare ederiz ve method body'sine ihtiyac kalmaz
+     */
+}
