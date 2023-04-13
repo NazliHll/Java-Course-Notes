bfb1eb488afd9b949f6b66b4a54df22c6a1af761
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-13 10:45:16 +0300

    13.04.2023

--- src/day40_final/CAbstract.java
+++ src/day40_final/CAbstract.java
@@ -2,21 +2,14 @@ package day40_final;
 
 public abstract class CAbstract {
 
-void toplama(){
+    void toplama(){
     System.out.println("bu method toplama yapar");
 }
-void carpma(){}
-
-
-
-
-
-
-
-
-
-
-
+    abstract void carpma();
+    abstract void bolme();
+    void cikarma(){
+        System.out.println("bu method cikarma yapar");
+    }
       /*
     Bir method'un basina abstract yazarsaniz bu method'un child class'lar icin bir
     standard oldugunu declare ederiz ve method body'sine ihtiyac kalmaz
--- src/day40_final/DChildOfAbstract.java
+++ src/day40_final/DChildOfAbstract.java
@@ -1,4 +1,20 @@
 package day40_final;
 
-public class DChildOfAbstract {
+public class DChildOfAbstract extends CAbstract{
+
+    @Override
+    void carpma() {
+        /*
+        Abstract class'lar child class'larin sahip olmalari gereken
+        mecburi ozellikleri belirler.
+        (Gunluk hayatımızdaki standardlar gibi)
+        Abstract bir class'i parent edinen tum child'lar parent abstract class'daki
+        tum abstract method'lari override etmek zorundadirlar.
+         */
+    }
+
+    @Override
+    void bolme() {
+
+    }
 }
