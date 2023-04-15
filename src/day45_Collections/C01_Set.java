069767660e9bd6e28481abb322f09a9fd56b4589
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-15 11:32:33 +0300

    15.04.2023

--- src/day45_Collections/C01_Set.java
+++ src/day45_Collections/C01_Set.java
@@ -1,4 +1,34 @@
 package day45_Collections;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class C01_Set {
+    public static void main(String[] args) {
+        /*
+        Verilen bir array'deki tekrar eden elementleri silip tekrarsiz
+        halini array'e atayan kod yaziniz.
+         */
+        int arr[] = {4, 5, 3, 6, 8, 5, 1, 9, 0, 4, 2, 5, 7, 9, 1, 2, 5, 7, 6};
+        Set<Integer> tekrarsizSet=new HashSet<>();
+
+        for (int each:arr
+             ) {
+            tekrarsizSet.add(each);
+        }
+        System.out.println(tekrarsizSet);//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        int []tekrarsizArr=new int[tekrarsizSet.size()];
+
+        int i=0;
+        for (Integer each:tekrarsizSet
+             ) {
+            tekrarsizArr[i]=each;
+            i++;
+        }
+        arr=tekrarsizArr;
+        System.out.println("bizim arrayin son hali: "+Arrays.toString(arr));//[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    }
 }
--- src/day45_Collections/C02_CollectionsDataTuru.java
+++ src/day45_Collections/C02_CollectionsDataTuru.java
@@ -1,4 +1,45 @@
 package day45_Collections;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class C02_CollectionsDataTuru {
+    public static void main(String[] args) {
+    /*
+        Collections <DataTuru>'nu Object secmeniz durumunda Collection'a farkli
+        data turlerinden objeler koymamiza izin verir.
+        Bu depolama acisindan bize esneklik saglar.
+        Ancak islem yaparken tum objeleri Object olarak tanimladigimizdan cok fazla
+        casting yapmamiz gerekebilir.
+         */
+
+        List<String> liste = new ArrayList();
+        /*
+        liste.add(5);
+        liste.add('s');
+        liste.add(true);
+         */
+
+        List<Object> list = new ArrayList();
+        list.add(5);
+        list.add('s');
+        list.add(true);
+        list.add("Ali");
+        System.out.println(list);//[5, s, true, Ali]
+
+        list.set(0,(Integer)list.get(0)+5);
+        list.set(3,list.get(3)+"can");
+        list.set(2,!(Boolean)list.get(2));
+        System.out.println(list);//[10, s, false, Alican]
+
+
+        Set<Object> set1=new HashSet<>();
+        set1.add(5);
+        set1.add(true);
+        set1.add('5');
+
+
+    }
 }
