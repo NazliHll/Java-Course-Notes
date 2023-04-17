d56bca0c4436a9f183840da6730a9bc973a98757
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-17 10:39:43 +0300

    17.04.2023

--- src/day47_Maps/C05_EntryUpdate.java
+++ src/day47_Maps/C05_EntryUpdate.java
@@ -1,5 +1,36 @@
 package day47_Maps;
 
-public class C05_EntryUpdate
-{
+import day46_Maps.ReusableMethods;
+
+import java.util.Map;
+import java.util.Set;
+
+
+public class C05_EntryUpdate {
+    public static void main(String[] args) {
+         /*
+        Soyisim Can olanlarin bransini DataScience yapalim.
+         */
+
+        Map<Integer, String> sinifListMap = ReusableMethods.mapOlustur();
+
+        /*
+        Entryleri alalim
+         */
+        Set<Map.Entry<Integer,String>> entrySeti=sinifListMap.entrySet();
+
+        String entryValue;
+        String[]entryArr;
+        for (Map.Entry<Integer,String>entry:entrySeti
+             ) {
+            entryValue=entry.getValue();
+            entryArr=entryValue.split(", ");
+
+            if (entryArr[1].equals("Can")){
+                entryArr[2]="DataScience";
+            }
+            entry.setValue(entryArr[0]+","+entryArr[1]+", "+entryArr[2]);
+        }
+        System.out.println(sinifListMap);
+    }
 }
